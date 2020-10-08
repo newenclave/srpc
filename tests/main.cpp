fe17a528da61e5994581c555b793ff2433352ee2
newenclave <pikrat.acid@gmail.com>
2020-10-08 14:01:36 +0300

    --

--- tests/main.cpp
+++ tests/main.cpp
@@ -11,6 +11,7 @@
 #include "srpc/common/layer_list.h"
 #include "srpc/common/packint/fixint.h"
 #include "srpc/common/packint/varint.h"
+#include "srpc/common/layers/pack_unpack.h"
 
 #ifdef _WIN32
 
@@ -41,155 +42,7 @@ using packer = srpc::common::packint::varint<std::size_t>;
 using namespace srpc::common;
 
 template <typename PackerT>
-class unpacker_layer : public layer<std::string, std::string> {
-
-    using packer_type = PackerT;
-    using packer_size_type = typename packer_type::size_type;
-
-public:
-    void on_upper_data(std::string msg) override
-    {
-        std::string result
-            = packer_type::pack(static_cast<packer_size_type>(msg.size()));
-        result.append(msg.begin(), msg.end());
-        write_down(std::move(result));
-    }
-
-    void on_lower_data(std::string msg) override
-    {
-        const auto check = [this](auto next) {
-            return (std::get<0>(next) > 0)
-                && collector_.size() >= (std::get<0>(next) + std::get<1>(next));
-        };
-
-        collector_.insert(collector_.end(), msg.begin(), msg.end());
-
-        auto next = PackerT::unpack(collector_.begin(), collector_.end());
-        while (check(next)) {
-            collector_.erase(collector_.begin(),
-                             collector_.begin() + std::get<0>(next));
-            msg.assign(collector_.begin(),
-                       collector_.begin() + std::get<1>(next));
-            collector_.erase(collector_.begin(),
-                             collector_.begin() + std::get<1>(next));
-
-            next = PackerT::unpack(collector_.begin(), collector_.end());
-
-            write_up(std::move(msg));
-        }
-    }
-
-private:
-    std::deque<char> collector_;
-};
-
-class pass_layer : public layer<std::string, std::string> {
-    void on_upper_data(std::string msg)
-    {
-        write_down(std::move(msg));
-    }
-    void on_lower_data(std::string msg)
-    {
-        write_up(std::move(msg));
-    }
-};
-
-class echo_layer : public layer<std::string, std::string> {
-    void on_upper_data(std::string msg)
-    {
-        write_down(std::move(msg));
-    }
-    void on_lower_data(std::string msg)
-    {
-        write_down(std::move(msg));
-    }
-};
-
-class hello_layer_client : public layer<std::string, std::string> {
-    void on_upper_data(std::string msg) override
-    {
-        write_down(std::move(msg));
-    }
-    void on_lower_data(std::string msg) override
-    {
-        if (verified_ > 0) {
-            collector_.append(msg);
-            auto n = collector_.find('\n');
-            if (n != std::string::npos) {
-                std::cout << std::string { collector_.begin(),
-                                           collector_.begin() + n }
-                          << "\n";
-                verified_--;
-                if (0 == verified_) {
-                    write_up({ collector_.begin() + n + 1, collector_.end() });
-                } else {
-                    collector_
-                        = { collector_.begin() + n + 1, collector_.end() };
-                }
-            }
-
-        } else {
-            write_up(std::move(msg));
-        }
-    }
-    int verified_ = 2;
-    std::string collector_;
-};
-
-class hello_layer_server : public layer<std::string, std::string> {
-public:
-    void on_upper_data(std::string msg) override
-    {
-        if (verified_) {
-            write_down(std::move(msg));
-        }
-    }
-
-    void on_lower_data(std::string msg) override
-    {
-        call_(std::move(msg));
-    }
-
-    void init()
-    {
-        call_ = [this](std::string msg) { hello(std::move(msg)); };
-        write_down("Hello there. Who are you?\r\n");
-    }
-
-    void hello(std::string msg)
-    {
-        collect_.append(msg);
-        if (collect_.find('\n') == std::string::npos) {
-            return;
-        }
-        auto f = collect_.find(':');
-        if (f == std::string::npos) {
-            write_down("try again!\r\n");
-            collect_.clear();
-            return;
-        }
-        std::string_view user { collect_.c_str(), f };
-        std::string_view password { collect_.c_str() + f + 1,
-                                    collect_.size() - f - 1 };
-
-        verified_ = true;
-        write_down("Welcome " + std::string { user.begin(), user.end() }
-                   + "\r\n");
-        collect_.clear();
-        call_ = [this](std::string msg) { pass(std::move(msg)); };
-    }
-
-    void pass(std::string msg)
-    {
-        write_up(std::move(msg));
-    };
-
-    std::function<void(std::string)> call_
-        = [this](std::string msg) { hello(std::move(msg)); };
-
-    std::string collect_;
-    bool verified_ = false;
-};
+using unpacker_layer = layers::pack_unpack<PackerT>;
 
 template <typename Lt>
 void echo_thread_server(SOCKET s, Lt client)
@@ -239,10 +92,7 @@ void start_server(std::uint16_t port)
         sai.sin_family = AF_INET;
         socklen_t addrlen = sizeof(sai);
         SOCKET s2 = accept(ls, reinterpret_cast<sockaddr *>(&rec), &addrlen);
-        auto lll = make_layer_list(echo_layer {}, unpacker_layer<packer> {},
-                                   hello_layer_server {});
-        thread_list.emplace_back(echo_thread_server<decltype(lll)>, s2,
-                                 std::move(lll));
+        break;
     }
 
     for (auto &t : thread_list) {
@@ -278,33 +128,17 @@ void start_client(std::uint16_t port)
 
     std::string test = "1234567890";
 
-    auto layers = make_layer_list(pass_layer {}, unpacker_layer<packer> {},
-                                  hello_layer_client {});
-    layers.on_lower_ready_connect(
-        [ls](auto msg) { send(ls, msg.c_str(), msg.size(), 0); });
-    layers.on_upper_ready_connect([&test](auto msg) {
-        if (test != msg) {
-            std::cout << msg << "\n";
-        }
-    });
-
     connect(ls, reinterpret_cast<sockaddr *>(&sai), sizeof(sai));
     int sent = send(ls, "f:f\n", 4, 0);
-    std::thread t(echo_client_thread<decltype(layers)>, ls, std::ref(layers));
-
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 1000000; ++i) {
-        layers.write_upper(test);
+        //layers.write_upper(test);
     }
 
     shutdown(ls, 2);
     closesocket(ls);
 
-    if (t.joinable()) {
-        t.join();
-    }
     auto stop = std::chrono::high_resolution_clock::now();
     std::cout << "time: "
               << std::chrono::duration_cast<std::chrono::milliseconds>(stop
@@ -313,56 +147,9 @@ void start_client(std::uint16_t port)
               << "\n";
 }
 
-namespace {
-    using namespace srpc::common;
-    class echo: public layer<std::string, int> {
-        void on_upper_data(std::string msg) override {
-            write_down(std::atoi(msg.c_str()));
-            write_up(std::move(msg));
-        }
-        void on_lower_data(int msg) override {
-            write_up(std::to_string(msg));
-            write_down(msg);            
-        }
-        
-    };
-    class i2f: public layer<int, float> {
-        void on_upper_data(int msg) {
-            write_down(msg);
-        }
-        void on_lower_data(float msg) {
-            write_up(msg);            
-        }
-    };
-    class f2c: public layer<float, std::string> {
-        void on_upper_data(float msg) override {
-            write_down(std::to_string(msg));
-        }
-        void on_lower_data(std::string msg) override {
-            write_up(std::atof(msg.c_str()));
-        } 
-    };
-}
-
 int main(int arg, char *argv[])
 {
-    auto layers = make_layer_list(echo{}, i2f{}, f2c{});
-    layers.on_ready_connect([](auto msg){
-        std::cout << "UL " << msg << "\n";
-    }, [](auto msg){
-        std::cout << "LL " << msg << "\n";
-    });
-    
-    std::string data;
-    layers.get<1>() = i2f{};
-    layers.write_lower(std::move(data));
-    
-    while(true) {
-        std::cin >> data;
-        layers.write_lower(std::move(data));
-    }
-    
-    return 0;
+
 #ifdef _WIN32
     WSADATA wsad {};
     if (SOCKET_ERROR == WSAStartup(0x0202, &wsad)) {
